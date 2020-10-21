ac0ad5987a7e5d994aaaa684d3ffdaaeae104d5e
Will Macrae <wmacrae@palantir.com>
2020-10-20 18:29:56 -0700

    Updates

---
+++ precache-manifest.b2b5770f3625e6e421f4fc81d76093d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5b3e3a1a37cb2081f0098209223780",
+    "url": "/mtg-analyzer/index.html"
+  },
+  {
+    "revision": "a4fa8aca5f27681c7d27",
+    "url": "/mtg-analyzer/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "9e1dd193c5be975f5f79",
+    "url": "/mtg-analyzer/static/css/main.f09315ee.chunk.css"
+  },
+  {
+    "revision": "a4fa8aca5f27681c7d27",
+    "url": "/mtg-analyzer/static/js/2.91d1848a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/mtg-analyzer/static/js/2.91d1848a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1dd193c5be975f5f79",
+    "url": "/mtg-analyzer/static/js/main.0a6ab658.chunk.js"
+  },
+  {
+    "revision": "4648a725943737f5d518",
+    "url": "/mtg-analyzer/static/js/runtime-main.766b1336.js"
+  }
+]);

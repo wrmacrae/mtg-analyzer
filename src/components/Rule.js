769ff04e74be5ac3d29ad8b130eb5cc7c774af6a
Will Macrae <wmacrae@palantir.com>
2020-10-21 10:22:58 -0700

    remove extra meta div

--- src/components/Rule.js
+++ src/components/Rule.js
@@ -30,17 +30,15 @@ class Rule extends React.PureComponent {
         element="li"
         className={width > 75 ? 'passed' :'failed'}
         header={(
-          <div className="meta">
-            <div className="flex wrappable">
-              <div className="flex-main name">{name}</div>
-              <div className="flex-sub perf">
-                <div className="perf-container">
-                  <div className="perf-bar" style={{width: width + "%", backgroundColor: color}}></div>
-                </div>
+          <div className="flex wrappable">
+            <div className="flex-main name">{name}</div>
+            <div className="flex-sub perf">
+              <div className="perf-container">
+                <div className="perf-bar" style={{width: width + "%", backgroundColor: color}}></div>
               </div>
-              <div className="flex-sub chevron">
-                <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9" className="icon"><path d="M14.0059397,2.26485497e-14 C15.1072288,2.26485497e-14 15.3722573,0.627746582 14.5881847,1.41182411 L8.70999225,7.29005337 C8.317902,7.68214607 7.67653023,7.6764822 7.29009898,7.29005337 L1.41183294,1.41182411 C0.632099139,0.632095183 0.894513756,2.26485497e-14 1.99406028,2.26485497e-14 L14.0059397,2.26485497e-14 Z"></path></svg></div>
-              </div>
+            </div>
+            <div className="flex-sub chevron">
+              <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9" className="icon"><path d="M14.0059397,2.26485497e-14 C15.1072288,2.26485497e-14 15.3722573,0.627746582 14.5881847,1.41182411 L8.70999225,7.29005337 C8.317902,7.68214607 7.67653023,7.6764822 7.29009898,7.29005337 L1.41183294,1.41182411 C0.632099139,0.632095183 0.894513756,2.26485497e-14 1.99406028,2.26485497e-14 L14.0059397,2.26485497e-14 Z"></path></svg></div>
             </div>
           </div>
         )}
